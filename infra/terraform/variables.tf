1f7fd9a7abd37bed19aaccfbfaa91a602c8b781a
ivana-meshed <ivana@mesheddata.com>
2025-08-25 13:59:21 +0200

    change var name

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "project_id" { default = "datawarehouse-422511" }
 variable "region" { default = "europe-west1" }
-variable "service_name" { default = "mmm-trainer" }
+variable "service_name" { default = "mmm-trainer-sa" }
 variable "image" { default = "europe-west1-docker.pkg.dev/datawarehouse-422511/mmm-repo/mmm-app:latest" }
 variable "bucket_name" { default = "mmm-app-output" }
