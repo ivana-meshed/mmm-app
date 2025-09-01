1cb1c382e2b301379fbd5c750818080c126991d7
ivana-meshed <ivana@mesheddata.com>
2025-09-01 18:12:01 +0200

    add training timeout

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -219,6 +219,7 @@ resource "google_cloud_run_v2_job" "training_job" {
   template {
     template {
       service_account = google_service_account.training_job_sa.email
+      timeout         = "21600s"
       max_retries     = 1
 
       containers {
