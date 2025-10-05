c1046364d351591b0bc4108ccef8a1d65f5b7742
ivana-meshed <ivana@mesheddata.com>
2025-10-06 00:02:58 +0200

    rm fct

--- r/run_all.R
+++ r/run_all.R
@@ -812,7 +812,7 @@ df_for_robyn <- sanitize_for_robyn(
     paid_media_spends = paid_media_spends,
     paid_media_vars = paid_media_vars,
     context_vars = context_vars,
-    factor_vars = context_vars,
+    # factor_vars = context_vars,
     organic_vars = organic_vars
 )
 
@@ -979,7 +979,7 @@ InputCollect <- withCallingHandlers(
             paid_media_spends = paid_media_spends,
             paid_media_vars = paid_media_vars,
             context_vars = context_vars,
-            factor_vars = context_vars,
+            # factor_vars = context_vars,
             organic_vars = organic_vars,
             window_start = min(df_for_robyn$date),
             window_end = max(df_for_robyn$date),
@@ -1318,7 +1318,7 @@ run_once <- function(InputCollect, iter = 200, trials = 1, ts_validation = TRUE,
                     InputCollect       = InputCollect,
                     iterations         = iter,
                     trials             = trials,
-                    ts_validation      = ts_validation,
+                    ts_validation      = TRUE,
                     add_penalty_factor = TRUE,
                     cores              = parallel::detectCores()
                 )
