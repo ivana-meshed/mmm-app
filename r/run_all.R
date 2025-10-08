4a24db86733234713bb4d3fc96f11365b19ce0d8
ivana-meshed <ivana@mesheddata.com>
2025-10-08 15:28:53 +0200

    factor

--- r/run_all.R
+++ r/run_all.R
@@ -808,7 +808,7 @@ df_for_robyn <- sanitize_for_robyn(
     paid_media_spends = paid_media_spends,
     paid_media_vars = paid_media_vars,
     context_vars = context_vars,
-    factor_vars = context_vars,
+    factor_vars = factor_vars,
     organic_vars = organic_vars
 )
 
